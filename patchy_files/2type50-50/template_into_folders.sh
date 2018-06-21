2ded47c9b47967fdd601be351c771bd5c6ca9413
ramansbach <mansbac2@illinois.edu>
2018-06-21 14:12:06 -0500

    updated 50/50 two type HOOMD code and helper code

--- patchy_files/2type50-50/template_into_folders.sh
+++ patchy_files/2type50-50/template_into_folders.sh
@@ -2,25 +2,33 @@
 #simple bash script that puts the correct script in each folder
 #for runs 1-5 with changing AA/SC well depth and ERAD (SC LJ sigma)
 
-for A in 250 500 750
+for A in 250
 do
-	for SC in 09
+	for SCA in 09
 	do
-		for SIG in 100 125 150 175
+		for SCB in 09
 		do
-			if [ ! -d "$A-$SC-$SIG" ]; then
-				mkdir $A-$SC-$SIG
+		for SIGA in 100
+		do
+		for SIGB in 100 125 150 175
+			do
+			if [ ! -d "$A-$SCA-$SCB-$SIGA-$SIGB" ]; then
+				mkdir $A-$SCA-$SCB-$SIGA-$SIGB
 			fi
 			for RUN in 1 2 3 4 5
 			do
-				python template_patterning.py $A $SC $SIG $RUN
-				mv production_template_temp.py $A-$SC-$SIG/production_run$RUN.py
-				sed "s/RUN/$RUN/g" production_template.pbs > $A-$SC-$SIG/production_run$RUN.pbs
-				sed "s/AA/$A/g" -i $A-$SC-$SIG/production_run$RUN.pbs
-				sed "s/SC/$SC/g" -i $A-$SC-$SIG/production_run$RUN.pbs
-				sed "s/SIG/$SIG/g" -i $A-$SC-$SIG/production_run$RUN.pbs
-				cp rundebughelpers.py $A-$SC-$SIG
-			done		
+				python template_patterning.py $A $SCA $SIGA $SCB $SIGB $RUN
+				mv production_template_temp.py $A-$SCA-$SCB-$SIGA-$SIGB/production_run$RUN.py
+				sed "s/RUN/$RUN/g" production_template.pbs > $A-$SCA-SCB-$SIGA-$SIGB/production_run$RUN.pbs
+				sed "s/AA/$A/g" -i $A-$SCA-$SCB-$SIGA-$SIGB/production_run$RUN.pbs
+				sed "s/SCA/$SCA/g" -i $A-$SCA-$SCB-$SIGA-$SIGB/production_run$RUN.pbs
+				sed "s/SCB/$SCB/g" -i $A-$SCA-$SCB-$SIGA-$SIGB/production_run$RUN.pbs
+				sed "s/SIGA/$SIGA/g" -i $A-$SCA-$SCB-$SIGA-$SIGB/production_run$RUN.pbs
+				sed "s/SIGB/$SIGB/g" -i $A-$SCA-$SCB-$SIGA-$SIGB/production_run$RUN.pbs
+				cp rundebughelpers.py $A-$SCA-$SCB-$SIGA-$SIGB
+			done	
+			done	
+		done
 		done
 
 	done
--- patchy_files/2type50-50/template_patterning.py
+++ patchy_files/2type50-50/template_patterning.py
@@ -1,9 +1,11 @@
 #python script that just simply updates a template file based on the given three inputs
 import re,sys
 A = sys.argv[1]
-SC = sys.argv[2]
-SIG = sys.argv[3]
-RUN = sys.argv[4]
+SCA = sys.argv[2]
+SIGA = sys.argv[3]
+SCB = sys.argv[4]
+SIGB = sys.argv[5]
+RUN = sys.argv[6]
 templatefile = "production_template.py"
 template = open(templatefile)
 tlines = template.readlines()
@@ -11,20 +13,30 @@ template.close()
 outfile = "production_template_temp.py"
 outtemp=open(outfile,'w')
 Afloat = float(A)/100.0
-SCfloat = float(SC)
-if SC == '02':
-    SCfloat = 0.2
-if SC == '09':
-    SCfloat = 0.9
-SIGfloat = float(SIG)/100.0
+SCAfloat = float(SCA)
+SCBfloat = float(SCB)
+if SCA == '02':
+    SCAfloat = 0.2
+if SCB == '02':
+    SCBfloat = 0.2
+if SCA == '09':
+    SCAfloat = 0.9
+if SCB == '09':
+    SCBfloat = 0.9
+SIGAfloat = float(SIGA)/100.0
+SIGBfloat = float(SIGB)/100.0
 for line in tlines:
     line = line.replace('SSS','12345')
     line = line.replace('=AAA','='+str(Afloat))
-    line = line.replace('=SCSCSC','='+str(SCfloat))
-    line = line.replace('=ERAD','='+str(SIGfloat))
+    line = line.replace('[SCSCSCA','['+str(SCfloatA))
+    line = line.replace('SCSCSCB]',str(SCfloatB)+']')
+    line = line.replace('[ERADA','['+str(SIGfloatA))
+    line = line.replace('ERADB]',str(SIGfloatB)+']')
     line = line.replace('_AAA','_'+A)
-    line = line.replace('-SCSCSC','-'+SC)
-    line = line.replace('-ERAD','-'+SIG)
+    line = line.replace('-SCSCSCA','-'+SCA)
+    line = line.repalce('-SCSCSCB','-'+SCB)
+    line = line.replace('-ERADA','-'+SIGA)
+    line = line.replace('-ERADB','-'+SIGB)
     line = line.replace('RUN',RUN)
     outtemp.write(line)
 outtemp.close()
